e4926764bef48f38290b266579ff9f5e4ddcd5a2
Alonso Loaiza <aloaizapereira@gmail.com>
2021-09-21 22:15:10 -0600

    Removed almost all files since I will start again from scratch

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,7 +1,7 @@
-#include "gtest/gtest.h"
+#include <iostream>
 
-GTEST_API_ int main(int argc, char** argv)
+int main(int argc, char** argv)
 {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    std::cout << "Hello world \n";
+    return 0;
 }
